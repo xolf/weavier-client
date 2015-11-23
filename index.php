893eafb97305b16ac5a6820fb2d5f877d7281f5c
xolf UG iG <xolf@users.noreply.github.com>
2015-11-23 23:06:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,52 @@ $a = $argv;
 
 $g = $_GET;
 
+$p = $_POST;
+
 $version = '0.0.1';
 
 $curl = curl_init();
 
-echo $version;
+
 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 curl_setopt($curl, CURLOPT_USERAGENT, 'weavier-client v' . $version);
 
+//Web interface
 if($g['action'] == 'setup')
 {
     
-    echo '<a href="?action=setup"></a>';
+?>
+	<h1>Weavier</h1>
+	<form method="post" action="?action=setup-create">
+		<input type="url" name="server" placeholder="Custom server address">
+		<p>Leave it blank if you want to use the standart weavier server</p>
+		<input type="submit" value="Setup">
+	</form>
+<?php    
     
+}
+
+if($g['action'] == 'setup-create')
+{
+
+	$client_url = $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
+	
+	curl_setopt($curl, CURLOPT_URL, '');
+	
+	$server_url = 'http://localhost/server/';
+	
+	if($p['server'] != "") $server_url = $p['server'];
+	
+	$add_url = $server_url . '?add-client=' . $client_url;
+	
+	curl_setopt($curl, CURLOPT_URL, $add_url);
+	
+	echo $add_url;
+	
+	//echo curl_exec($curl);
+	
+	echo curl_error($curl);
+
 }
