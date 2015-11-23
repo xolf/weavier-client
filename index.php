9911de2bcb0f8f30ba36fba4e83490bfb92c99b8
xolf UG iG <xolf@users.noreply.github.com>
2015-11-23 20:51:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,18 @@
 $a = $argv;
 
 $g = $_GET;
+
+$version = '0.0.1';
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+curl_setopt($curl, CURLOPT_USERAGENT, 'weavier-client v' . $version);
+
+if($g['action'] == 'setup')
+{
+    
+    echo '<a href="?action=setup"></a>';
+    
+}
